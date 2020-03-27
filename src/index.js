8d288217df17a121b533a5c90c1b3ab3724d8901
wylkson <marcosduval123@gmail.com>
2020-03-27 19:45:58 -0300

    Criação de API para controle de ongs, casos e usuários. Também foi criado a persistência em SQLite

---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const env = require('../environment')
+const routes = require('./routes');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(env.PORT);
